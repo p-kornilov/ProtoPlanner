1b3c21dd48ab83614f08935f5b426a2c36ad6c79
p-kornilov <kornilov.p@gmail.com>
2017-12-20 23:24:08 +0300

    ttt

--- app/src/main/java/com/vividprojects/protoplanner/Interface/NavigationController.java
+++ app/src/main/java/com/vividprojects/protoplanner/Interface/NavigationController.java
@@ -7,12 +7,27 @@ import javax.inject.Inject;
  */
 
 public class NavigationController {
-    public final static int MODE_PHONE = 0;
-    public final static int MODE_TABLET = 1;
-    private final int mode;
+    public final static int MODE_PHONE = 1;
+    public final static int MODE_TABLET = 2;
+    private int mode;
 
     @Inject
-    public NavigationController(int mode) {
+    public NavigationController() {
+        this.mode = MODE_PHONE;
+    }
+
+    public void init(int mode) {
         this.mode = mode;
+    }
+
+    public void openRecord(String id) {
+        switch (mode) {
+            case MODE_PHONE:
+                // open new ViewRecord (id)
+                break;
+            case MODE_TABLET:
+                // replace fragment record (id)
+                break;
+        }
     }
 }
