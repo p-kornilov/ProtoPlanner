4107bf80ab2778c1b9b38b0a2b1af992fe8f1024
p.kornilov <kornilov.p@gmail.com>
2018-03-02 15:20:43 +0300

    qqq

---
+++ app/src/main/java/com/vividprojects/protoplanner/Widgets/PrefixedEditText.java
@@ -0,0 +1,44 @@
+package com.vividprojects.protoplanner.Widgets;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+/**
+ * Created by p.kornilov on 02.03.2018.
+ */
+
+public class PrefixedEditText extends AppCompatEditText {
+    private String mPrefix = ""; // can be hardcoded for demo purposes
+    private Rect mPrefixRect = new Rect(); // actual prefix size
+
+    public PrefixedEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        getPaint().getTextBounds(mPrefix, 0, mPrefix.length(), mPrefixRect);
+        mPrefixRect.right += getPaint().measureText(" "); // add some offset
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawText(mPrefix, super.getCompoundPaddingLeft(), getBaseline(), getPaint());
+    }
+
+    @Override
+    public int getCompoundPaddingLeft() {
+        return super.getCompoundPaddingLeft() + mPrefixRect.width();
+    }
+
+    public void setPrefix(String prefix) {
+        mPrefix = prefix;
+        requestLayout();
+    }
+}
