005a78d51f0c376c9f337ea8e28741a33e4d8d0d
p.kornilov <kornilov.p@gmail.com>
2018-05-30 10:34:48 +0300

    qqq

---
+++ app/src/main/java/com/vividprojects/protoplanner/Interface/Helpers/DialogFullScreenActivity.java
@@ -0,0 +1,6 @@
+package com.vividprojects.protoplanner.Interface.Helpers;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class DialogFullScreenActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/vividprojects/protoplanner/Interface/Helpers/DialogFullScreenDialogAbstract.java
@@ -0,0 +1,4 @@
+package com.vividprojects.protoplanner.Interface.Helpers;
+
+public class DialogFullScreenDialogAbstract {
+}
