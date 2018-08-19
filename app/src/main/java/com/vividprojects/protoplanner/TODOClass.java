a93049f78ad46733f35ca9f5cdddafcbbd02cc9f
p-kornilov <kornilov.p@gmail.com>
2018-08-19 21:05:00 +0300

    ttt

---
+++ app/src/main/java/com/vividprojects/protoplanner/TODOClass.java
@@ -0,0 +1,6 @@
+package com.vividprojects.protoplanner;
+
+public class TODOClass {
+    //TODO 1. Добавление/удаление/редактирование группы label
+    //TODO 2. Добавление/удаление/редактирование label в группе
+}
